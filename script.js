d2a763985829298e9084b190e47dfc78c79e1e53
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-27 20:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let countval = document.getElementById("counter");
+const btn = document.getElementById("incrementBtn");
+let count = 0;
+
+countval.textContent = count;
+
+btn.onclick = function () {
+    alert(count);
+    count++;
+    countval.textContent = count;
+};
